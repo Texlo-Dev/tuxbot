53744f4590b8e815ea7f5e150285ba1905d3e231
RTK99 <richrancy@gmail.com>
2017-04-21 11:37:15 -0400

    New changes

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
@@ -12,11 +11,18 @@ client.on('message', msg => {
   }
 });
 
-client.on('message', msg => {
-  if (msg.content === 'windows') {
-    msg.reply('Please do not mention that here!');
+fs = require('fs')
+fs.readFile('./discord_key.txt', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
   }
+  var api_key = data;
+  console.log(data);
 });
 
-
-client.login('MzA0NjI5OTUzMDU0NTA3MDE4.C9raMg.n6MCux9Vr05nNNlYY3PR_8Z5dHQ');
+if (api_key) {
+  client.login(api_key);
+}
+{
+  console.log("No API Key found");
+}
