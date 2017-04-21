c489ec9304c7aee926327debaad84233ca5807b2
RTK99 <richrancy@gmail.com>
2017-04-21 12:14:50 -0400

    updated file

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
 var api_key;
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
 });
@@ -18,11 +19,10 @@ fs.readFile('./discord_key.txt', 'utf8', function (err,data) {
     return console.log(err);
   }
   api_key = data;
+  if (api_key) {
+  client.login(api_key);
+    } else {
+  console.log("No API Key found");
+    }
   console.log(data);
 });
-
-if (api_key) {
-  client.login(api_key);
-} else {
-  console.log("No API Key found");
-}
