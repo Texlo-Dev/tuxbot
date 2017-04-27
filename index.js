f2b6b61eadc80957a9cc4c071ac6321175e5d311
RTK99 <richrancy@gmail.com>
2017-04-26 20:30:48 -0400

    updated from server

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require('fs');
+var prefix = "/"
 var api_key;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
+  client.user.setGame('with a Terminal');
 });
 
 client.on('message', msg => {
@@ -17,6 +19,20 @@ client.on("message", msg=> {
   if (msg.content.startsWith("Tux")) {
     msg.channel.sendMessage("Did someone mention me?");
   }
+});
+
+client.on("message", msg=> {
+   if (msg.content === '/fetchavatar') {
+     msg.reply(msg.author.avatarURL);
+ }
+});
+
+client.on("message", msg=> {
+  if (msg.content.startsWith(prefix + 'adddistro')) {
+    var nickname = msg.content.replace(prefix + "adddistro", "")
+    msg.member.setNickname(msg.author.username + "[" + nickname + "]");
+    msg.reply("Nickname Set.");
+ }
 });
 
 fs.readFile('./discord_key.txt', 'utf8', function (err,data) {
