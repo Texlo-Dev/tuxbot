15bf4672ad2cd5095da942c786964789df5ad745
rTexel <rramce3@outlook.com>
2017-09-23 07:51:10 -0400

    distro roles!

--- commands/distro/distro.js
+++ commands/distro/distro.js
@@ -1,6 +1,7 @@
 const { Command } = require('discord.js-commando');
 const {Database, Model} = require('mongorito');
 const connection = new Database('localhost/tuxbot');
+let roles = ['Ubuntu', 'Kali', 'Debian', 'Arch', 'OpenSUSE', 'RedHat', 'Fedora', 'Others', 'Manjaro', 'Antergos'];
 connection.connect()
     .then(() => console.log('Connected to MongoDB'))
     .catch(err => console.error(`Hmm..there was an error connecting with MongoDB.. ${err.stack}`));
@@ -23,16 +24,28 @@ module.exports = class DistroCommand extends Command {
             args: [{
                 key: 'distro',
                 prompt: 'What distro would you like to add?',
-                type: 'string',
-                default: ''
+                type: 'role',
+                validate: t => {
+                    if (roles.includes(t)) return true;
+                    return 'Sorry, this isn\'t a valid distro role.';
+                    
+                }
+
             }]
         });
     }
 
     async run (message, { distro }) {
+        message.member.addRole(distro).catch(e => {
+            return message.reply('Sorry, this role could not be assigned.');
+        });
+        message.reply(`You have been successfully updated to **${distro.name}** :thumbsup:`);
+
+
+        /*
         if (!distro) {
             message.member.setNickname(message.author.username);
-            return message.reply('Your distro has been reset!');
+            return message.reply('Your role has been reset!');
         }
         let newNick;
         var nickname = message.content.replace(`./distro`, '').trim();
@@ -44,6 +57,6 @@ module.exports = class DistroCommand extends Command {
         });
         if (!toAdd) return message.reply(`Sorry, the distro **${distro}** didn't exist. If you know its correct, contact a server admin to get it added.`);
         message.member.setNickname(newNick).catch(e => message.reply('Sorry, I cannot change your nickname.'));
-        message.reply(`You have been successfully updated to **${distro}** :thumbsup:`);
+        message.reply(`You have been successfully updated to **${distro}** :thumbsup:`); */
     }
 };
