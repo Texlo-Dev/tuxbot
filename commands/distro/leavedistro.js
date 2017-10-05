91345d7362486108aa44a36948501c7b7ec253e7
rTexel <rramce3@outlook.com>
2017-10-05 19:50:10 -0400

    Added remove distro command

--- commands/distro/leavedistro.js
+++ commands/distro/leavedistro.js
@@ -11,7 +11,7 @@ module.exports = class DistroCommand extends Command {
             example: ['distro ubuntu'],
             args: [{
                 key: 'distro',
-                prompt: 'What distro would you like to add?',
+                prompt: 'What distro would you like to leave?',
                 type: 'role',
                 default: ''
 
@@ -22,12 +22,12 @@ module.exports = class DistroCommand extends Command {
     async run (message, { distro }) {
         if (!distro) return message.reply('Sorry, no distro was specicied.');
         if (!roles.includes(distro.name)) return message.reply('Sorry, that wasn\'t a valid distro role.');
-        if (message.channel.id !== '361120040524972032') return message.channel.send('All roles should be requested in #role-request.');
+        if (message.channel.id !== '361120040524972032') return message.channel.send('All role removals should be requested in #role-request.');
         message.delete();
-        message.member.addRole(distro).catch(e => {
-            return message.reply('Sorry, this role could not be assigned.');
+        message.member.removeRole(distro).catch(e => {
+            return message.reply('Sorry, this role could not be removed.');
         });
-        message.reply(`You have been successfully updated to **${distro.name}** :thumbsup:`);
+        message.reply(`You have been successfully removed from **${distro.name}** :thumbsup:`);
 
     }
 };
